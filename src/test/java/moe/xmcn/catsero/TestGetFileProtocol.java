c062b94c59566d96cae0b307d1962a83f28fbc55
BaimoQilin <86581525+Zhou-Shilin@users.noreply.github.com>
2023-04-13 17:20:38 +0800

    Delete src directory

--- src/test/java/moe/xmcn/catsero/TestGetFileProtocol.java
+++
@@ -1,9 +0,0 @@
-package moe.xmcn.catsero;
-
-import moe.xmcn.catsero.utils.HttpClient;
-
-public class TestGetFileProtocol {
-    public static void main(String[] args) {
-        new HttpClient().getRequest("file://E:/Downloads/database.json");
-    }
-}
